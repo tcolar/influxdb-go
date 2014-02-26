b0e1d2abe69ca8216f9f9e290814e526d6f8dcdc
Thibaut Colar <tcolar@colar.net>
2014-02-25 23:02:06 -0800

    Support for series merging, for easy custom graphing

--- custom.go
+++ custom.go
@@ -47,6 +47,49 @@ func (s *Series) PtVal(target interface{}, colName string) (err error) {
 	return err
 }
 
+// SeriesMerge creates a merged series from the provided series (merge by "time")
+// All series should be based on the same time base for this to be useful
+// ColMapping is used to specify which columns we want returned and what to name them.
+// It is indexed the same as the series it refers to.
+func SeriesMerge(name string, series []*Series, colMapping []map[string]string) *Series {
+	allCols := []string{"time"}
+	merged := Series{
+		Name: name,
+	}
+	pts := map[float64][]interface{}{}
+	for i, s := range series {
+		indexes := []int{}
+		for k, v := range colMapping[i] {
+			index := s.ColIndex(k)
+			if index != -1 {
+				allCols = append(allCols, v)
+				indexes = append(indexes, index)
+			}
+		}
+		for _, point := range s.Points {
+			pt := []interface{}{}
+			for _, index := range indexes {
+				pt = append(pt, point[index])
+			}
+			time := point[0].(float64)
+			for len(pts[time]) < i { // padding
+				pts[time] = append(pts[time], []interface{}{0.0})
+			}
+			pts[time] = append(pts[time], pt...)
+		}
+	}
+	merged.Columns = allCols
+	for time, vals := range pts {
+		pt := []interface{}{time}
+		pt = append(pt, vals...)
+		for len(pt) < len(allCols) { // padding
+			pt = append(pt, 0.0)
+		}
+		merged.Points = append(merged.Points, pt)
+	}
+	return &merged
+}
+
 // ColIndex finds the Index of a column in the series
 // -1 if not found
 func (s *Series) ColIndex(colName string) int {
